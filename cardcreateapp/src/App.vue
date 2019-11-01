e64d52c0ed1295f76e8b1abbb8aa0a9ed4a575ca
Aloysio Ventura <aloysio@nibo.com.br>
2019-10-31 21:33:18 -0300

    iniciando app

---
+++ cardcreateapp/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <app-welcome></app-welcome>
+  </div>
+</template>
+
+<script>
+import Welcome from './components/Welcome.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'app-welcome': Welcome
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ cardcreateapp/src/components/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+<h1>Let's create your app card!</h1>
+    <button class="btn">
+            Begin!
+    </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
